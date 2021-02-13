01d1d9098f3c8625495ab045865562a7126525f7
dianakakoma <dikakoma@gmail.com>
2021-02-13 10:41:46 -0600

    installed Boostrap and created the navigation bar

--- tutorial.py
+++ tutorial.py
@@ -2,14 +2,11 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html",content = [1,2,3],r="go", name="Robert")
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home", name="User!"))
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
+    #app.run(debug=TRUE) - generate error - "NameError: name 'TRUE' is not defined"
     app.run()
